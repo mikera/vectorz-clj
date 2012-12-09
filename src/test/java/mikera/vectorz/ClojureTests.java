f617c096cdef2d95c7c4a94901c921ede99bb241
mikera <mike.r.anderson.13@gmail.com>
2012-12-09 09:48:27 +0800

    Widen cljunit tests

--- src/test/java/mikera/vectorz/ClojureTests.java
+++ src/test/java/mikera/vectorz/ClojureTests.java
@@ -1,18 +1,7 @@
 package mikera.vectorz;
 
-import java.util.Arrays;
-import java.util.List;
-
 import mikera.cljunit.ClojureTest;
 
 public class ClojureTests extends ClojureTest {
-
-	@Override
-	public List<String> namespaces() {
-		return Arrays.asList(new String[] {
-			"mikera.vectorz.test-core",
-			"mikera.vectorz.test-matrix"			
-		});
-		
-	}
+	// test all namespaces by default
 }
