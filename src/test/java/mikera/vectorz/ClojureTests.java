76a80fa3dfb05e5d8cef8f294a7b5d874b21ea64
mikera <mike.r.anderson.13@gmail.com>
2013-01-20 11:37:41 +0800

    Update to latest cljunit and add namespace filter for tests

--- src/test/java/mikera/vectorz/ClojureTests.java
+++ src/test/java/mikera/vectorz/ClojureTests.java
@@ -3,5 +3,10 @@ package mikera.vectorz;
 import mikera.cljunit.ClojureTest;
 
 public class ClojureTests extends ClojureTest {
-	// test all namespaces by default
+	
+	@Override
+	public String filter() {
+		return "mikera";
+	}
+	
 }
