9a57feb7be79aaf058cde6824a7cba055e0088dc
mikera <mike.r.anderson.13@gmail.com>
2013-03-07 18:11:57 +0800

    benchmark enhancements

--- src/test/clojure/mikera/vectorz/benchmark_matrix.clj
+++ src/test/clojure/mikera/vectorz/benchmark_matrix.clj
@@ -28,21 +28,25 @@
         b [1 2 3]]
     (c/quick-bench (dotimes [i 1000] (add a b))))  
   
+  ;; Adding two regular Clojure vectors with clojure.core/+
   (let [a [1 2 3 4 5 6 7 8 9 10]
         b [1 2 3 4 5 6 7 8 9 10]]
     (c/quick-bench (dotimes [i 1000] (vec (map clojure.core/+ a b)))))  
   ;; => Execution time mean per addition : 1308 ns
   
+  ;; Adding two core.matrix vectors (pure functions, i.e. creating a new vector)
   (let [a (matrix :vectorz [1 2 3 4 5 6 7 8 9 10])
         b (matrix :vectorz [1 2 3 4 5 6 7 8 9 10])]
     (c/quick-bench (dotimes [i 1000] (+ a b))))
   ;; => Execution time mean per addition: 68 ns
   
+  ;; Adding two core.matrix vectors (mutable operation, i.e. adding to the first vector)
   (let [a (matrix :vectorz [1 2 3 4 5 6 7 8 9 10])
         b (matrix :vectorz [1 2 3 4 5 6 7 8 9 10])]
     (c/quick-bench (dotimes [i 1000] (add! a b))))
   ;; => Execution time mean per addition: 36 ns
   
+  ;; Adding two core.matrix vectors using low level Java interop
   (let [a (Vectorz/create [1 2 3 4 5 6 7 8 9 10])
         b (Vectorz/create [1 2 3 4 5 6 7 8 9 10])]
     (c/quick-bench (dotimes [i 1000] (.add a b))))
