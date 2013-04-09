4436d011a654c6d93f7129af420f1c708d4117d3
mikera <mike.r.anderson.13@gmail.com>
2013-04-09 16:43:21 +0800

    remove affine namespace

--- src/main/clojure/mikera/vectorz/affine.clj
+++
@@ -1,7 +0,0 @@
-(ns mikera.vectorz.affine
-  (:import [mikera.vectorz AVector Vectorz Vector Vector3])
-  (:import [mikera.matrixx AMatrix Matrixx Matrix])
-  (:import [mikera.transformz ATransform])
-  (:require [mikera.vectorz.core :as v]))
-
-;; TODO: put affine transform functionality in here? or merge into matrix.clj?
