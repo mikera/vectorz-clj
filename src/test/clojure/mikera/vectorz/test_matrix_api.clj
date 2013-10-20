8dd52e0a073bd0f304adfadd68ea02c625b5084b
mikera <mike.r.anderson.13@gmail.com>
2013-10-20 12:23:59 +0800

    use latest vectorz, update tests

--- src/test/clojure/mikera/vectorz/test_matrix_api.clj
+++ src/test/clojure/mikera/vectorz/test_matrix_api.clj
@@ -65,8 +65,8 @@
 (deftest test-broadcasting-cases
   (is (equals [[2 3] [4 5]] (add (array [[1 2] [3 4]]) (array [1 1]))))
   (is (equals [[2 3] [4 5]] (add (array [1 1]) (array [[1 2] [3 4]]))))
-  (is (equals [[2 4] [6 8]] (mul (array :vectorz [[1 2] [3 4]]) (array :ndarray 2))))
-  (is (equals [[2 6] [6 12]] (mul (array :vectorz [[1 2] [3 4]]) (array :ndarray [2 3])))))
+  (is (equals [[2 4] [6 8]] (mul (array :vectorz [[1 2] [3 4]]) (scalar-array 2))))
+  (is (equals [[2 6] [6 12]] (mul (array :vectorz [[1 2] [3 4]]) [2 3]))))
 
 (deftest test-broadcasts
   (is (equals [[2 2] [2 2]] (broadcast 2 [2 2]))))
