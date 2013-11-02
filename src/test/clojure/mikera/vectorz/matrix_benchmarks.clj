be857584e45e3abe8139e64cf99e424a68547794
mikera <mike.r.anderson.13@gmail.com>
2013-11-02 15:15:48 +0800

    comment out benchmarks to stop running at test time

--- src/test/clojure/mikera/vectorz/matrix_benchmarks.clj
+++ src/test/clojure/mikera/vectorz/matrix_benchmarks.clj
@@ -13,27 +13,29 @@
 
 (set-current-implementation :vectorz)
 
-;; elementwise mutation of 10x10 matrix, followed by computation of the sum
-;; => about 3,000 ns
-(defn buildsum [n]
- (let [bv (zero-matrix n n)]
-   (dotimes [i n]
-     (dotimes [j n]
-       (mset! bv i j (* i j))))
-   (esum bv)))
-(c/quick-bench (buildsum 10))
+(defn benchmarks []
+	;; elementwise mutation of 10x10 matrix, followed by computation of the sum
+	;; => about 3,000 ns
+	(defn buildsum [n]
+	 (let [bv (zero-matrix n n)]
+	   (dotimes [i n]
+	     (dotimes [j n]
+	       (mset! bv i j (* i j))))
+	   (esum bv)))
+	(c/quick-bench (buildsum 10))
+	
+	
+	;; multiplication of two 100x100 matrices
+	;; => about 1.3 ms
+	(defn multiply [n]
+	  (let [ma (zero-matrix n n)
+	        mb (zero-matrix n n)]
+	    (mmul ma mb)))
+	(c/quick-bench (multiply 100))
+	
+	
+	;; multiplication of two 10x10 matrices
+	;; => about 1,800 ns
+	(c/quick-bench (multiply 10))
 
-
-;; multiplication of two 100x100 matrices
-;; => about 1.3 ms
-(defn multiply [n]
-  (let [ma (zero-matrix n n)
-        mb (zero-matrix n n)]
-    (mmul ma mb)))
-(c/quick-bench (multiply 100))
-
-
-;; multiplication of two 10x10 matrices
-;; => about 1,800 ns
-(c/quick-bench (multiply 10))
-
+)
