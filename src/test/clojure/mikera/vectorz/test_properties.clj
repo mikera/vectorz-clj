af895f69c52bdd5a4ab3ef1294b5a6c295b54d7d
mikera <mike.r.anderson.13@gmail.com>
2014-05-03 11:30:33 +0100

    start of generator code

--- src/test/clojure/mikera/vectorz/test_properties.clj
+++ src/test/clojure/mikera/vectorz/test_properties.clj
@@ -5,3 +5,15 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :as ct :refer (defspec)])
   (:import [mikera.vectorz AVector Vectorz Vector]))
+
+(set-current-implementation :vectorz)
+
+;; generator for legitimate shapes for vectors
+;; vector sizes grow linearly
+(def gen-vector-shape
+  (gen/fmap vector gen/pos-int))
+
+;; generator for legitimate shapes of arrays. We don't allow zero-length dimensions
+;; element counts grow linearly
+(def gen-shape
+  :TODO)
