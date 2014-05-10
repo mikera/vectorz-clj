cc01a5aac9169e1858d4964071fa045ceb7d1aa4
mars0i <mabrams@uab.edu>
2014-05-09 21:24:48 -0500

    Updated project.clj to reflect pom.xml.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 ;; The pom.xml is used for official builds, and should be considered the 
 ;; definitive source for build configuration.
 
-(defproject net.mikera/vectorz-clj "0.20.1-SNAPSHOT"
+(defproject net.mikera/vectorz-clj "0.22.1-SNAPSHOT"
   :description "Fast vector library for Clojure, building on VectorZ and using core.matrix"
   :url "https://github.com/mikera/vectorz-clj"
   :license {:name "GNU Lesser General Public License (LGPL)"
@@ -12,6 +12,8 @@
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [criterium/criterium "0.4.3"]
+                 [org.clojure/tools.analyzer "0.1.0-beta12"]
+                 [org.clojure/test.check "0.5.7"]
                  [net.mikera/cljunit "0.3.0"]
                  [net.mikera/clojure-utils "0.6.0"]
                  [net.mikera/core.matrix "0.23.0"]
