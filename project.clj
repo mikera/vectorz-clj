d6b921cbe971e6d91f9ed16c7941e1a35acf9f27
mabrams <mabrams@uab.edu>
2014-08-17 11:42:55 -0500

    Updated project.clj from pom.xml.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 ;; The pom.xml is used for official builds, and should be considered the 
 ;; definitive source for build configuration.
 
-(defproject net.mikera/vectorz-clj "0.24.1-SNAPSHOT"
+(defproject net.mikera/vectorz-clj "0.25.0"
   :description "Fast vector library for Clojure, building on Vectorz and using core.matrix"
   :url "https://github.com/mikera/vectorz-clj"
   :license {:name "GNU Lesser General Public License (LGPL)"
@@ -12,12 +12,12 @@
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [criterium/criterium "0.4.3"]
-                 [org.clojure/tools.analyzer "0.4.0"]
-                 [org.clojure/test.check "0.5.8"]
+                 [org.clojure/tools.analyzer "0.5.1"]
+                 [org.clojure/test.check "0.5.9"]
                  [net.mikera/cljunit "0.3.1"]
                  [net.mikera/clojure-utils "0.6.1"]
-                 [net.mikera/core.matrix "0.28.0"]
+                 [net.mikera/core.matrix "0.29.1"]
                  [net.mikera/core.matrix.stats "0.4.0"]
-                 [net.mikera/vectorz "0.39.0"]]
+                 [net.mikera/vectorz "0.40.0"]]
   :repositories [["clojars.org" {:url "https://clojars.org/repo"
                                  :name "Clojars repository"}]])
