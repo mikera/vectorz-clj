60826c5514669aecc2cd9c79cdadf6c63db99c51
Mike <mike@mikera.net>
2015-05-25 14:07:59 +0800

    add generators namespace

---
+++ src/test/clojure/mikera/vectorz/generators.clj
@@ -0,0 +1,6 @@
+(ns mikera.vectorz.generators
+  (:require [clojure.core.matrix :as m]
+            [clojure.test.check.generators :as gen]
+            [clojure.core.matrix.generators :as gm]
+            [clojure.test.check.properties :as prop]))
+
