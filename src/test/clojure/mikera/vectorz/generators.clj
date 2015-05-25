ec26a7db21aa2e52632d3a06a80b36bafbe479b5
Mike <mike@mikera.net>
2015-05-25 14:57:01 +0800

    more generator code

--- src/test/clojure/mikera/vectorz/generators.clj
+++ src/test/clojure/mikera/vectorz/generators.clj
@@ -5,12 +5,25 @@
             [clojure.test.check.properties :as prop]
             [mikera.cljutils.error :refer [error]]))
 
-(defn mutable-vector 
-  "Create a generator for fully mutable matrices of the gien shape"
+(defn subvector
+  "Creates a subvector generator from a vector generator"
+  ([g-vector]
+    (gen/bind g-vector
+              (fn [v]
+                (let [n (m/ecount v)]
+                  ())))))
+
+(defn mutable-array
+  "Create a generator for fully mutable arrays"
   ([]
-    (gen/bind (gen/choose 1 1) 
-             (fn [i] 
-               (case i 
-                       1 (gm/gen-vector gm/gen-double :vectorz)
-                       (error "Incorrect generator type: " i))))))
+    (gen/one-of 
+      [(gm/gen-array (gm/gen-shape) gm/gen-double :vectorz)])))
+
+(defn mutable-vector 
+  "Create a generator for fully mutable vectors
+"
+  ([]
+    (gen/one-of 
+      [(gm/gen-vector gm/gen-double :vectorz)
+       (gen/fmap m/as-vector (mutable-array))])))
 
