4a07d9783522a87853119caa7d6bf23f8767bd93
Mike <mike@mikera.net>
2015-05-25 14:37:23 +0800

    tidy code

--- src/test/clojure/mikera/vectorz/generators.clj
+++ src/test/clojure/mikera/vectorz/generators.clj
@@ -2,5 +2,15 @@
   (:require [clojure.core.matrix :as m]
             [clojure.test.check.generators :as gen]
             [clojure.core.matrix.generators :as gm]
-            [clojure.test.check.properties :as prop]))
+            [clojure.test.check.properties :as prop]
+            [mikera.cljutils.error :refer [error]]))
+
+(defn mutable-vector 
+  "Create a generator for fully mutable matrices of the gien shape"
+  ([]
+    (gen/bind (gen/choose 1 1) 
+             (fn [i] 
+               (case i 
+                       1 (gm/gen-vector gm/gen-double :vectorz)
+                       (error "Incorrect generator type: " i))))))
 
