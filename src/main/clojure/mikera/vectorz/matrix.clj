974cc312f2af8cb7b323bff83dc5675d0fc49476
Mike <mike@mikera.net>
2016-01-03 23:23:14 +0000

    Add checks for boxing

--- src/main/clojure/mikera/vectorz/matrix.clj
+++ src/main/clojure/mikera/vectorz/matrix.clj
@@ -15,7 +15,7 @@
   (:refer-clojure :exclude [* get set zero?]))
   
 (set! *warn-on-reflection* true)
-(set! *unchecked-math* true)
+(set! *unchecked-math* :warn-on-boxed)
 
 ;; ============================================
 ;; Core functions
