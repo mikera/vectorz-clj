6a51e32568806493df58f4991d3952cf52a161be
mikera <mike.r.anderson.13@gmail.com>
2017-09-02 14:17:36 +0800

    Optimise applyTo for primitive fn ops on strided vectors

--- src/main/java/mikera/vectorz/PrimitiveFnOp.java
+++ src/main/java/mikera/vectorz/PrimitiveFnOp.java
@@ -27,7 +27,21 @@ public final class PrimitiveFnOp extends mikera.vectorz.ops.AFunctionOp {
 	@Override 
 	public void applyTo(double[] xs, int offset, int length) {
 		for (int i=0; i<length; i++) {
-			xs[offset+i]=fn.invokePrim(xs[offset+i]);
+			int ii=offset+i;
+			xs[ii]=fn.invokePrim(xs[ii]);
+		}
+	}
+	
+	@Override
+	public void applyTo(double[] data, int offset, int stride, int length) {
+		if (stride==1) {
+			applyTo(data,offset,length);
+		} else {
+			for (int i=0; i<length; i++) {
+				int ii=offset+i*stride;
+				double x=data[ii];
+				data[ii]=fn.invokePrim(x);
+			}
 		}
 	}
 
