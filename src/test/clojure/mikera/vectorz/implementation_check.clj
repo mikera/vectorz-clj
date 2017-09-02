91991585dbc513425814dd70dadaaca47fe35253
mikera <mike.r.anderson.13@gmail.com>
2017-09-02 14:59:34 +0800

    Docs and tidying for implementation-check functionality

--- src/test/clojure/mikera/vectorz/implementation_check.clj
+++ src/test/clojure/mikera/vectorz/implementation_check.clj
@@ -1,5 +1,6 @@
 (ns mikera.vectorz.implementation-check
   (:use clojure.core.matrix)
+  (:require [clojure.test :as test])
   (:require [clojure.core.matrix.utils :as utils]))
 
 (set! *warn-on-reflection* true)
@@ -7,14 +8,16 @@
 
 (set-current-implementation :vectorz)
 
-(defn test-impls []
-	(array [1])
-	
-	(def protos (utils/extract-protocols))
- 
-  (into {} (mapv #(do [% (utils/unimplemented %)])
-                 [mikera.arrayz.INDArray
-                  mikera.matrixx.AMatrix
-                  mikera.vectorz.AVector
-                  mikera.vectorz.AScalar]))
-)
+(def protos (utils/extract-protocols))
+
+(defn test-impls 
+  "Gets a map of vectorz types to unimplemented interfaces.
+   Intended to allow us to check which protocols still need to be implemented."
+  ([]
+    (array :vectorz [1])
+    (into {} (mapv #(do [% (utils/unimplemented %)])
+                   [mikera.arrayz.INDArray
+                    mikera.matrixx.AMatrix
+                    mikera.vectorz.AVector
+                    mikera.vectorz.AScalar]))))
+
